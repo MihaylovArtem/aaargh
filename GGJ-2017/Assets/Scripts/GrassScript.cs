f49ce8927af98462f296139d0a103dfe4e9606c4
YIvan <megavirus74@gmail.com>
2017-01-21 12:54:25 +0500

    grass grass grass grass grass grass

---
+++ GGJ-2017/Assets/Scripts/GrassScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class GrassScript : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+        var generator = new System.Random();
+        foreach (var sprite in sprites)
+        {
+            var spriteRenderer = this.GetComponent<SpriteRenderer>();
+            spriteRenderer.sprite = sprites[generator.Next(sprites.Count())];
+        }          
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
