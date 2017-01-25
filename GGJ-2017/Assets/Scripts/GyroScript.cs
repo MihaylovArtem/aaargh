708d6e2c13a2e0b8d2f773973e68cb27d8c5a509
Волков Иван Алексеевич <volkoivan74@gmail.com>
2017-01-25 18:40:00 +0500

    Didn't save a file. oops

--- GGJ-2017/Assets/Scripts/GyroScript.cs
+++ GGJ-2017/Assets/Scripts/GyroScript.cs
@@ -3,18 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GyroScript : MonoBehaviour {
-	Vector3 angle = Vector3.zero;
-	//TextMesh text; <-- I was using a text mesh to test displaying
+	private float initialYAngle = 0f;
+	private float appliedGyroYAngle = 0f;
+	private float calibrationYAngle = 0f;
 
-	void Start() {
-		//text = GetComponent<TextMesh>();
+	void Start()
+	{
+		Input.gyro.enabled = true;
+		Application.targetFrameRate = 60;
+		initialYAngle = transform.eulerAngles.y;
+		CalibrateYAngle ();
 	}
 
-	void Update() {
-		angle.x = Input.acceleration.x * 360;
-		angle.y = Input.acceleration.y * 360;
-		angle.z = Input.acceleration.z * 360;
-		transform.rotation = Quaternion.Euler(angle);
-		//text.text = Input.acceleration.x.ToString();
+	void Update()
+	{
+		ApplyGyroRotation();
+		ApplyCalibration();
+	}
+
+	void OnGUI()
+	{
+	}
+
+	public void CalibrateYAngle()
+	{
+		calibrationYAngle = appliedGyroYAngle - initialYAngle; // Offsets the y angle in case it wasn't 0 at edit time.
+	}
+
+	void ApplyGyroRotation()
+	{
+		transform.rotation = Input.gyro.attitude;
+		transform.Rotate( 0f, 0f, 180f, Space.Self ); // Swap "handedness" of quaternion from gyro.
+		transform.Rotate( 90f, 90f, 0f, Space.World ); // Rotate to make sense as a camera pointing out the back of your device.
+		appliedGyroYAngle = transform.eulerAngles.y; // Save the angle around y axis for use in calibration.
+	}
+
+	void ApplyCalibration()
+	{
+		transform.Rotate( 0f, -calibrationYAngle, 0f, Space.World ); // Rotates y angle back however much it deviated when calibrationYAngle was saved.
 	}
 }
