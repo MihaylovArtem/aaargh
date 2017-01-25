33dd6d81a9b55ad62ccc0ca47ac3a46414233eba
Волков Иван Алексеевич <volkoivan74@gmail.com>
2017-01-25 18:36:34 +0500

    Added fix for iPhone speakers and fixed android build settings

--- GGJ-2017/Assets/Scripts/AudioInput.cs
+++ GGJ-2017/Assets/Scripts/AudioInput.cs
@@ -13,6 +13,7 @@ public class AudioInput : MonoBehaviour {
 	void InitMic(){
 		if(_device == null) _device = Microphone.devices[0];
 		_clipRecord = Microphone.Start(_device, true, 999, 44100);
+		iPhoneSpeaker.ForceToSpeaker ();
 	}
 
 	void StopMicrophone()
